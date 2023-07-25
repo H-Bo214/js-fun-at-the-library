ead02d3b83b0b013ec00b639b36dc58252d43b0a
Horacio Borrego <mac-m1@H-M1.local>
2023-07-25 13:26:45 -0700

    JavaScript practice

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,44 @@
+function shelfBook(book, shelfGenre) {
+  if (shelfGenre.length < 3) {
+    shelfGenre.unshift(book);
+  }
+  return shelfGenre;
+}
+
+function unshelfBook(bookTitle, shelfGenre) {
+  shelfGenre.forEach((book) => {
+    const bookToRemove = shelfGenre.indexOf(book);
+    if (book.title === bookTitle) {
+      shelfGenre.splice(bookToRemove, 1);
+    }
+  });
+}
+
+function listTitles(shelfGenre) {
+  const bookTitles = shelfGenre.reduce(
+    (bookNames, book) => (bookNames += `${book.title}, `),
+    ""
+  );
+  const formattedBookTitles = bookTitles.slice(0, bookTitles.lastIndexOf(","));
+  return formattedBookTitles;
+}
+
+function searchShelf(shelfGenre, bookTitle) {
+  let result = false;
+  shelfGenre.forEach((book) => {
+    if (book.title === bookTitle) {
+      result = true;
+    }
+  });
+  return result;
+  //iterate over shelfGenre
+  // access each book object's title
+  // if the title exists return true || false
+}
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf,
 };
